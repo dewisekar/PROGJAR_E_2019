96c67659699b9b24ae6d9b8531cb2e333d615553
dewisekar <sekar.6499@gmail.com>
2019-03-27 12:26:39 +0700

    server download test

--- Tugas 3/server.py
+++ Tugas 3/server.py
@@ -17,10 +17,51 @@ sock.listen(1)
 def handleRequest(connection, client_address):
     try:
         print>>sys.stderr, 'Connection from', client_address
+        req_msg = ''
+        while True:
+            data = connection.recv(32)
+            req_msg = req_msg + data
+            if (req_msg[-4:] == "\r\n\r\n"):
+                break
+
+        print req_msg
+        baris = req_msg.split("\r\n")
+        baris_request = baris[0]
+        print baris_request
+        a, url, c = baris_request.split(" ")
+
+        if (url.startswith("/download")):
+            respon = response_download(url)
+        elif (url == '/') :
+            respon = "HTTP/1.1 200 OK\r\n" \
+            "Content-Type: text/plain\r\n" \
+            "Content-Length: 19\r\n" \
+            "\r\n" \
+            "Connected to server"
+        else:
+            respon = "HTTP/1.1 200 OK\r\n" \
+                     "Content-Type: text/plain\r\n" \
+                     "Content-Length: 9\r\n" \
+                     "\r\n" \
+                     "Not found"
+
+        connection.send(respon)
 
     finally:
         print "Closing socket"
         sock.close()
+
+def response_download(req):
+    url, name = req.split("=")
+    print name
+    file = open(name, 'rb').read()
+    length = len(file)
+    hasil = "HTTP/1.1 200 OK\r\n" \
+            "Content-Type:  multipart/form-data\r\n" \
+            "Content-Length: {}\r\n" \
+            "\r\n" \
+            "{}".format(length, file)
+    return hasil
 
 while True:
     #wait a connection
